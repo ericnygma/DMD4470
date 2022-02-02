389918821c57d6e40d7f5bcbcd191d1f2b7d1367
Andy Gardner <xenygma@gmail.com>
2022-02-02 16:22:58 -0500

    add theme picker v1

--- ColorPicker/JS/theme.js
+++ ColorPicker/JS/theme.js
@@ -1,29 +1,33 @@
 var colorWell;
 var body = document.querySelector('body');
 var defaultColor = "#0000ff";
+const picker = document.getElementById('themePicker');
 
 window.addEventListener("load", startup, false);
-//test this line
 window.addEventListener("load", remember, false);
 
 function startup() {
   colorWell = document.querySelector("#colorWell");
-  //colorWell.value = defaultColor;
+  colorWell.value = defaultColor;
   colorWell.addEventListener("input", updateBody, false);
-  colorWell.addEventListener("change", updateBody, false);
+  colorWell.addEventListener("change", updateAll, false);
   colorWell.select();
-
+  
   
 }
 // updates the body color
 function updateBody(e){
-    //var b = document.querySelector("body");
-
     if(body){
         body.style.backgroundColor = e.target.value;
     
     }
 } 
+function updateAll(e) {
+    body.forEach(function(){
+        body.style.backgroundColor = e.target.value;
+    });
+}
+
 //Local Storage
 function remember(){
 if(!localStorage.getItem('backgroundColor')){
@@ -35,6 +39,8 @@ function populateStorage(){
     localStorage.setItem('backgroundColor', body.style.backgroundColor.value);
     setStyles();
 }
+//sets of from local storage
+
 function setStyles(){
     var currentColor = localStorage.getItem('backgroundColor');
 
@@ -44,7 +50,21 @@ function setStyles(){
 }
 }
 
+var selTheme = document.querySelector('.batman');
+ 
 
-const updateTheme = document.getElementsByClassName('updateTheme');
 
-updateTheme.classList
+//Changes theme
+
+picker[1].addEventListener("click", batman)
+picker[2].addEventListener("click", riddler);
+
+function batman() {
+    body.classList.add('batman');
+  console.log('im batman');
+}
+
+function riddler() {
+    body.classList.add('riddler');
+  console.log('im the riddler');
+}
