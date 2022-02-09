859b2c013471b1a41a1a7812958035c4e44c59bc
Andy Gardner <xenygma@gmail.com>
2022-02-09 08:59:54 -0500

    save progress

--- phonebook/js/main.js
+++ phonebook/js/main.js
@@ -7,16 +7,33 @@ async function myContacts(){
     const response = await fetch(url);
     const object = await response.json();
     const contact = await object.contacts;
+
     contact.forEach(obj => {
-        // console.log(obj.firstname);
-        document.createElement('p')
-        document.getElementById('contactInfo').innerHTML = obj.firstname;
-    })
+        document.getElementById('allContacts').innerHTML = `
+            <div>
+                <li>Name:<strong> ${obj.lastname}, ${obj.firstname} </strong></li>
+                <li>Email: ${obj.email}</li>
+               
+            </div>`;
+    
+    // contact.forEach(obj => {
+       
+        
+    //     console.log(obj);
+    //     let p = document.createElement('p')
+    //     document.getElementById('contactInfo').innerHTML = obj.firstname;
+    //     document.getElementById('contactInfo').innerHTML = obj.phone;
+    // })
     
     // console.log(contact[1].firstname);
     // console.log(contact.length);
+})
 }
 myContacts();
+
+
+
+
 
 
 class Person {
@@ -24,11 +41,12 @@ class Person {
         this.firstname = firstname;
         this.lastname = lastname;
         this.title = title;
-        this.phonenum = phone;
+        this.phone = phone;
         this.email = email;
         this.birthday = birthday;
+        this.fullname = this.lastname +", "+ this.firstname;
 
         phonebook.push(this)
     }
 }
-var contact = new Person();
+// var contact = new Person();
