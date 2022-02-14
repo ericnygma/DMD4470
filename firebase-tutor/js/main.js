8bc850f802dbf28428fce0597ad980a52a288bd8
Andy Gardner <xenygma@gmail.com>
2022-02-14 10:38:48 -0500

    clean up

---
+++ firebase-tutor/js/main.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCAEyWz_1AwDTFtXSaFO6U5a53Dwg42SAI",
+    authDomain: "web-quickstart-637a4.firebaseapp.com",
+    projectId: "web-quickstart-637a4",
+    storageBucket: "web-quickstart-637a4.appspot.com",
+    messagingSenderId: "561384612025",
+    appId: "1:561384612025:web:be81af0a7ac00a39bd3b68"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
---
+++ phonebookv2/js/main.js
@@ -0,0 +1,51 @@
+// Declare
+const url = 'https://uconndxlab.github.io/json-phonebook-example/dxlab-staff.json';
+const contactList = document.getElementById('cl');
+var phonebook = [];
+
+
+async function myPhonebook(){
+    const response = await fetch(url);
+    const object = await response.json();
+    const contact = await object.contacts;
+    console.log(contact);
+    contact.forEach(obj => {
+        contactList.innerHTML += `
+            <div class="card">
+                <li>Name:<strong> ${obj.lastname}, ${obj.firstname} </strong></li>
+                <li>Title: ${obj.title}</li>
+                <li>Phone: ${obj.phone}</li>
+                <li>Email: <a href='#'> ${obj.email} <a/></li>
+                <li>Birthday: ${obj.birthdate}</li>
+            </div>`;
+    
+    // contact.forEach(obj => {
+       
+        
+    //     console.log(obj);
+    //     let p = document.createElement('p')
+    //     document.getElementById('contactInfo').innerHTML = obj.firstname;
+    //     document.getElementById('contactInfo').innerHTML = obj.phone;
+    // })
+    
+    // console.log(contact[1].firstname);
+    // console.log(contact.length);
+})
+}
+myPhonebook();
+
+
+class Person {
+    constructor(firstname, lastname, title, phone, email, birthdate){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.title = title;
+        this.phone = phone;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.fullname = this.lastname +", "+ this.firstname;
+
+        phonebook.push(this)
+    }
+}
+// let contact = new Person();
