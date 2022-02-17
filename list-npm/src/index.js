8492f341a6f2fc03bd871fba186d0f923fdf970f
Andy Gardner <xenygma@gmail.com>
2022-02-17 16:39:53 -0500

    new direction

--- list-npm/src/index.js
+++ list-npm/src/index.js
@@ -1,28 +1,16 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
-import { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js";
+
+
 
 const firebaseApp = initializeApp({
     apiKey: "AIzaSyArkN3o_VY3AZJam-Irat65BJBTP1Nv_hg",
-  authDomain: "shoppinglist-677c0.firebaseapp.com",
-  databaseURL: "https://shoppinglist-677c0-default-rtdb.firebaseio.com",
-  projectId: "shoppinglist-677c0",
-  storageBucket: "shoppinglist-677c0.appspot.com",
-  messagingSenderId: "97325349541",
-  appId: "1:97325349541:web:835a45f01db61e59996b4c"
+    authDomain: "shoppinglist-677c0.firebaseapp.com",
+    databaseURL: "https://shoppinglist-677c0-default-rtdb.firebaseio.com",
+    projectId: "shoppinglist-677c0",
+    storageBucket: "shoppinglist-677c0.appspot.com",
+    messagingSenderId: "97325349541",
+    appId: "1:97325349541:web:835a45f01db61e59996b4c"
 });
 const db = getFirestore(firebaseApp);
-// db.collection('todos').getDocs();
-// const todosCol = collection(db, 'todos');
-// const snapshot = await getDocs(todosCol);
 
-
-try {
-    const docRef = await addDoc(collection(db, "users"), {
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
